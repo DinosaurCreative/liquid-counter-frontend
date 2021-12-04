ac528c0f1a6d22b6d4662b4ba9bc28496a382752
German <gimalaevgerman@yandex.ru>
2021-12-04 22:00:33 +0300

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+
+function App () {
+
+  return (
+    <div className="page">
+      
+      <Routes>
+
+      </Routes>
+      
+    </div>
+  );
+};
+
+export default App;
