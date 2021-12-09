2950e3a02f179ff6ffb1f851e255f0c23d4f8f8b
German <gimalaevgerman@yandex.ru>
2021-12-09 19:57:08 +0300

    feat: make footer 1280px layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 // import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from '../Header/Header.js';
+import Footer from '../Footer/Footer';
 
 function App () {
 
   return (
     <div className='page'>
       <Header />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+
+
+function Footer() {
+
+  return (
+    <div className='footer'>
+      <div className='footer__container'>
+        <p className='footer__copyrights'>&copy; 2021 Гимаев Герман</p>
+        <ul className='footer__list'>
+          <a className='link link_place_footer' href='/' target='self'>Ссылка</a>
+          <a className='link link_place_footer' href='/' target='self'>Ссылка</a>
+          <a className='link link_place_footer' href='/' target='self'>Ссылка</a>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
