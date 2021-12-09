83492aa26da59d37f1a697b8ca184a18d7c8a4af
German <gimalaevgerman@yandex.ru>
2021-12-10 00:02:51 +0300

    feat: create register layout 1280px

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import Header from '../Header/Header.js';
 import Footer from '../Footer/Footer';
+import Registration from '../Registration/Registration';
 
 function App () {
 
   return (
     <div className='page'>
-      <Header />
-      <Footer />
+      {/* <Header />
+      <Footer /> */}
+      <Registration />
     </div>
   );
 };
---
+++ src/components/Registration/Registration.js
@@ -0,0 +1,22 @@
+function Registration() {
+  return (
+    // <div className='registration'>
+      <div className='form__container'>
+        <h1 className='form__title'>{'Регистрация'}</h1>
+        <form className='form' type='submit'>
+          <input className='form__input' type='text' placeholder='Имя'/>
+          <span className='form__error-span'>Чета не то</span>
+          <input className='form__input' type='email' placeholder='E-mail'/>
+          <span className='form__error-span'></span>
+          <input className='form__input' type='Password' placeholder='Пароль'/>
+          <span className='form__error-span'></span>
+          <input className='form__input' type='text' placeholder='Секретный ключ'/>
+          <span className='form__error-span'></span>
+          <button className='form__submit-button' type='submit'>{'Зарегистрироваться'}</ button>
+        </form>
+      </div>
+    // </div>
+  )
+}
+
+export default Registration;
