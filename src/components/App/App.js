20d2abaaf73b80d4108cf3eb6a890469f3d8394a
German <gimalaevgerman@yandex.ru>
2021-12-16 22:38:45 +0300

    feat: create smoothly sliding side menu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,17 @@
-// import { Route, Routes } from 'react-router-dom';
-import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { prevInventaTitle, bottlesDBTitle } from '../../utils/constants';
 
 import './App.css';
 import Header from '../Header/Header.js';
 import Footer from '../Footer/Footer';
-// import Registration from '../Registration/Registration';
-// import Login from '../Login/Login';
-// import Main from '../Main/Main';
-// import AddItem from '../AddItem/AddItem';
-// import UpdateItem from '../UpdateItem/UpdateItem';
+import Registration from '../Registration/Registration';
+import Login from '../Login/Login';
+import Main from '../Main/Main';
+import AddItem from '../AddItem/AddItem';
+import UpdateItem from '../UpdateItem/UpdateItem';
 import PreviousInventa from '../PreviousInventa/PreviousInventa';
-// import ItemDataBase from '../ItemDataBase/ItemDataBase';
+import ItemDataBase from '../ItemDataBase/ItemDataBase';
 import BottlesData from '../BottlesData/BottlesData';
 import ItemList from '../ItemList/ItemList';
 import { bottles, inventas } from '../../utils/constants';
@@ -20,7 +20,7 @@ import { bottles, inventas } from '../../utils/constants';
 
 function App () {
   const [ bottlesDB, setBottlesDB ] = useState(bottles);
-
+  const [ isMenuOpened, setIsMenuOpened ] = useState(false);
 
 
   // Внутренности компонента previousInventa
@@ -52,22 +52,24 @@ function App () {
     })
       setInventarizations([ ...sorted]);
       setSortedBy(id);
-  }
-  // Внутренности компонента previousInventa
-
+    }
+    // Внутренности компонента previousInventa
+    
 
   return (
     <div className='page'>
-      <Header />
-      {/* <ItemList component={PreviousInventa}
+      <Header  isMenuOpened = {isMenuOpened}
+               setIsMenuOpened = {setIsMenuOpened} />
+
+      <ItemList component={PreviousInventa}
                 data={inventarizations}
                 title = {prevInventaTitle}
                 sortByField = {sortByField}
-              /> */}
-      <ItemList component = {BottlesData} 
+              />
+      {/* <ItemList component = {BottlesData} 
                 data = {bottlesDB}
                 title = {bottlesDBTitle}
-                />
+                /> */}
       <Footer />
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,18 +2,43 @@ import './Header.css';
 import { Link } from 'react-router-dom'
 
 
-function Header() {
+function Header(props) {
 
+  function menuCloseHandler(e){
+    if (e.target.className.includes('header__navigation-menu') || e.key === 'Escape' || e.target.className === 'header__navigation-close-btn'){
+      document.removeEventListener('click', menuCloseHandler);
+      document.removeEventListener('keydown', menuCloseHandler);
+      props.setIsMenuOpened(false)
+    }
+  }
+
+  function menuOpenHandler() {
+    document.addEventListener('click', menuCloseHandler);
+    document.addEventListener('keydown', menuCloseHandler);
+    props.setIsMenuOpened(true)
+  }
+  
   return (
     <div className="header">
       <div className='header__container'>
         <Link to='/' href='#' className="logo logo_place_header" />
-
+        <nav className={`header__navigation-menu ${!props.isMenuOpened ? 'header__navigation-menu_closed' : ''}` }>
+          <div className={`header__navigation-container ${!props.isMenuOpened ? ' header__navigation-container_closed' : ''}`}>
+            <button className={'header__navigation-close-btn'} type='button'></button>
+            <h2 className='header__navigation-title'>Меню</h2>
+            <div className='header__navigation-link-container'>
+              <Link className='header__navigation-link' to='#'>Список инвентаризаций</Link>
+              <Link className='header__navigation-link' to='#'>База данных</Link>
+              <Link className='header__navigation-link' to='#'>Добавить позицию</Link>
+              <Link className='header__navigation-link' to='#'>Начать инвентаризацию</Link>
+            </div>
+          </div>
+        </nav>
         <div className="header__link-container">
           {/* <Link to='signin' href='#' className='link link_place_header-signup' >{'РЕГИСТРАЦИЯ'}</ Link>
           <Link to='signup' href='#' className='link link_place_header-signin' >{'ВОЙТИ'}</ Link> */}
         </div>
-        <button className='header__menu-btn' type='button' />
+        <button className='header__menu-btn' onClick={menuOpenHandler} type='button' />
 
       </div>
     </div>
