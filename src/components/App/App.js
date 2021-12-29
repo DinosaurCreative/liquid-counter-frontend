f8082ce2a91bc111d9c81a3705b4e6a2a9f2221d
German <gimalaevgerman@yandex.ru>
2021-12-29 16:32:51 +0300

    feat: create prev inventa layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -61,7 +61,7 @@ function App () {
       <Header  isMenuOpened = {isMenuOpened}
                setIsMenuOpened = {setIsMenuOpened} />
       <ItemList component = {Inventa}
-                data = {inventa}
+                data = {inventas}
                 title = {inventaTitle}
                 
                 
